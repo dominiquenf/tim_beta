699f286c34008c3ba120ec0be43838394f9f5777
Dominique Fonseca <dominiquenf@gmail.com>
2013-11-29 02:26:23 -0200

    Create tim_beta.h

---
+++ tim_beta.h
@@ -0,0 +1,18 @@
+@@ -0,0 +1,17 @@
++//
++//  tim_beta.h
++//  tim beta
++//
++//  Created by Dominique Fonseca on 18/11/13.
++//  Copyright (c) 2013 Dominique Fonseca. All rights reserved.
++//
++
++
++#ifndef tim_beta_tim_beta_h
++#define tim_beta_tim_beta_h
++
++int dias_mes(int _mes, int _ano);
++float beta(float sms, float internet, float voz);
++
++
++#endif
